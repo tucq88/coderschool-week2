bb9b2a0a2bd9ad0fbbd3011fcd247b1a7125b818
Tucq <tucq88@gmail.com>
2016-10-18 08:13:04 +0000

    Add fonts as assets for Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  # Custom assets
+  config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
