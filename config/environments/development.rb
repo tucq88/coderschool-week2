964b2323509f108976cfbc8fe35fe0b77fcf8b02
Tucq <tucq88@gmail.com>
2016-10-18 08:17:12 +0000

    Change additional assets path config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,9 +37,6 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
-  # Custom assets
-  config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
