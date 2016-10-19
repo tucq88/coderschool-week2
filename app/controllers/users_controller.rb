a6305ed089b4ea950a752206330afd70f5ff002f
Tucq <tucq88@gmail.com>
2016-10-19 17:25:13 +0000

    Add register form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+
+  # signup form
+  def new
+
+  end
+
+  # do signup
+  def store
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get 'login' => 'sessions#login'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
+
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#store'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
