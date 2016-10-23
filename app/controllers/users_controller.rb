1681e0dfcddbceabede40b8c788e46651ab7b3af
Tucq <tucq88@gmail.com>
2016-10-23 13:56:28 +0700

    Login after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'User registered successfully'
-      # TODO Sent confirmation email
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       flash[:error] = 'Invalid data. Check your inputs'
-      session[:user_id] = @user.id
       render 'new'
     end
   end
